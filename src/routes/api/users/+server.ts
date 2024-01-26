74dbb4cce7abb821741150c7f13c05e1f02c8b06
David Smeringe <david@matictribe.com>
2024-01-26 13:40:15 +0100

    fix query

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from "@sveltejs/kit";
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET({ request, platform }) {
   let result = await platform.env.DB.prepare(
-    "SELECT * FROM users LIMIT 5"
+    "SELECT * FROM customers LIMIT 5"
   ).run();
   return new Response(JSON.stringify(result));
 }
